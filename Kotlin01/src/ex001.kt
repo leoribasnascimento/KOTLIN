68875c957128dc0f88fa6bc3e2acf570b15e9b0d
Leonardo Ribas Nascimento <leoribas.freelancer@gmail.com>
2020-05-04 00:44:53 -0300

    update ex001 e criação do ambiente

---
+++ Kotlin01/src/ex001.kt
@@ -0,0 +1,25 @@
+/**
+ * Programa Ex001 do *Curso de Kotlin*
+ * @author Leonardo Ribas
+ * @since Mai-2020
+ * @param [args] Argumentos do programa
+ */
+
+fun main(args: Array<String>) {
+    println("Oi, \t\t\ttudo bem?")
+    println("Oi, \rtudo bem?")
+    println("Oi,\btudo bem?")
+    print("Oi,\\tudo bem?\n")
+    println("Oi,\"tudo\" bem?")
+
+    /*
+     * Enunciado: Crie um programa que mostre na tela
+     * uma mensagem utilizando quebra de linha e aspas.
+     * fim de tudo
+     * olha só
+     */
+    print("Kotlin é\n\"SUPER\"\nFácil!")
+    // Lembrando que \"significa "MOSTRE ASPAS NA TELA"
+
+
+}
